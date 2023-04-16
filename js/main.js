1ad76cd7a72e60deec4792e90852d2ba170d3f60
Gaston Benitez <gastonbenitez97.gb@gmail.com>
2023-04-16 16:31:39 -0300

    segunda pre-entrega

--- js/main.js
+++ js/main.js
@@ -1,49 +1,82 @@
+// clases
+class Tortas{
+    constructor(nombre, ingrediente1, ingrediente2, ingrediente3){
+        this.nombre = nombre;
+        this.ingrediente1 = ingrediente1;
+        this.ingrediente2 = ingrediente2;
+        this.ingrediente3 = ingrediente3;
+        this.precio = 1000;
+    }
+    calcularDescuento(){
+     return this.precio*0.9;
+    }
+    descripcionTortas() {
+        console.log("Genial! has hecho una torta " + this.nombre + " de " + this.ingrediente1 + ", " + this.ingrediente2 + " y " + this.ingrediente3 + " valor " + this.precio);
+    }
+    }
+
 // funciones
-function cargarIngredientes(ingrediente1, ingrediente2, ingrediente3) {
-    console.log("Genial! has elegido " + ingrediente1 + " como primer ingrediente");
-    console.log("Genial! has elegido " + ingrediente2 + " como segundo ingrediente");
-    console.log("Genial! has elegido " + ingrediente3 + " como tercer ingrediente");
-    alert("Perfecto! tu torta tiene los siguientes ingredientes: " + ingrediente1 + ", " + ingrediente2+ " y " + ingrediente3)
+function validarNombre() {
+    let nombre = prompt("cual es el nombre de esta torta?");
+ while (nombre == "" || nombre == null) {
+    alert("lo siento debes ingresar un nombre");
+    nombre = prompt("Porfavor ingrese un nombre valido");
 }
-function validarIngrediente(){
-   let ingrediente =prompt("Porfavor ingrese un ingrediente");
-while(ingrediente == "" || ingrediente== null){
-    alert("lo siento debes ingresar un ingrediente"); 
-   ingrediente = prompt("Porfavor ingrese un ingrediente valido");
+return nombre;
 }
-return ingrediente;
+
+function validarIngrediente() {
+        let ingrediente = prompt("Porfavor ingrese un ingrediente");
+     while (ingrediente == "" || ingrediente == null) {
+        alert("lo siento debes ingresar un ingrediente");
+        ingrediente = prompt("Porfavor ingrese un ingrediente valido");
+    }
+    return ingrediente;
 }
-function descripcionTortas(){
-  console.log("torta de " + ingrediente1 + ", " + ingrediente2+ " y " + ingrediente3+ " valor $1000");
+function cargarTortas(){
+        let nombreTorta = validarNombre();
+        let ingrediente1 = validarIngrediente();
+        let ingrediente2 = validarIngrediente();
+        let ingrediente3 = validarIngrediente();
+        objetoTorta = new Tortas(nombreTorta, ingrediente1, ingrediente2, ingrediente3);
+        contenedorTortas.push(objetoTorta);
+
+      
 }
-function valorTorta(cantidadTortas) {
-    return cantidadTortas * 1000;
+function validarCantidad() {
+    let cantidadTorta = parseInt(prompt("Cuantas tortas deseas preparar?"));
+    while (cantidadTorta <= 0) {
+        alert("porfavor ingrese una cantidad valida")
+        cantidadTorta = parseInt(prompt("Cuantas tortas deseas preparar?"));
+    }
+    return cantidadTorta;
 }
-function calcularDescuento(){
-   return valorTorta(cantidadTortas) * 0.9;
-}
+
+
 // titulo
 console.log("Bienvenido a nuestra pagina de tortas!");
 alert("Bienvenido a nuestra pagina de tortas!");
-alert("Pongamos manos a la obra y preparemos una torta!");
+alert("Pongamos manos a la obra y preparemos tortas con 3 ingredientes!");
 // estructura
-let ingrediente1 =validarIngrediente();
-let ingrediente2 =validarIngrediente();
-let ingrediente3 =validarIngrediente();
-
-
-cargarIngredientes(ingrediente1,ingrediente2, ingrediente3);
-// valor de las tortas
-let cantidadTortas= parseInt(prompt("El valor de cada torta es de $1000, cuantas quiere?"));
-
+let contenedorTortas = [];
+let cantidadTortas = validarCantidad();
 for (let i = 0; i < cantidadTortas; i++) {
-    descripcionTortas();
+    cargarTortas();
     }
-
-
-let descuento = prompt("Para acceder a un 10% de descuento ingrese el cupon 'DESCUENTO'") 
-if (descuento.toUpperCase() == "DESCUENTO") {
-    alert("El valor total de las tortas son $".concat( calcularDescuento()));
+contenedorTortas.forEach(torta => {
+    torta.descripcionTortas();
+});
+alert("Perfecto! has hecho " + cantidadTortas + " torta/s con un valor individual de $1000")
+let descuento = prompt("Para acceder a un 10% de descuento ingrese el cupon 'DESCUENTO'")
+if(descuento.toUpperCase() == "DESCUENTO") {
+    let totalDescuento = contenedorTortas.reduce((acumulador, actual)=> acumulador + actual.calcularDescuento(), 0)
+    alert("el total es de: $" + totalDescuento);
 }else{
-    alert("El valor total de las tortas son $".concat( valorTorta(cantidadTortas)));
+    let totalSinDescuento = contenedorTortas.reduce((acumulador, actual)=> acumulador + actual.precio, 0)
+    alert("el total es de: $" + totalSinDescuento);
 }
+
+
+
+
+
--- readme.md
+++ readme.md
@@ -3,6 +3,8 @@ simulador de armado de tortas
 # descripcion: 
 en este simulador podras crear una torta con 3 ingredientes, luego podras ingresar la cantidad de tortas que desees y adquirir un descuento del 10%
 # intrucciones
-1er paso ingresar 1 ingrediente en cadda prompt, luego ingresar la cantidad de tortas deseadas y por ultimo decidir si desea acceder al descuento con la palabra "descuento"
+1er paso ingresar la cantidad de tortas que desea hacer
+2do paso ingresar 1 ingrediente en cada prompt  
+3er decidir si desea acceder al descuento con la palabra "descuento"
 # autor
 Gaston Ivan Benitez
